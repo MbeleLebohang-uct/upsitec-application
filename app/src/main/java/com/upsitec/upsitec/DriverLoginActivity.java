c5e13f9e5ad79ac4de1e79fac67ee138bd057e0a
MbeleLebohang-uct <mblleb006@myuct.ac.za>
2018-07-06 12:25:45 +0200

    fixed login backend

--- app/src/main/java/com/upsitec/upsitec/DriverLoginActivity.java
+++ app/src/main/java/com/upsitec/upsitec/DriverLoginActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,6 +25,8 @@ public class DriverLoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    private ProgressBar mLoginProgressBar, mRegistrationProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,16 +55,29 @@ public class DriverLoginActivity extends AppCompatActivity {
         mLogin = findViewById(R.id.login_button);
         mRegistration = findViewById(R.id.registration_button);
 
+        mRegistrationProgressBar = findViewById(R.id.registration_progress_bar);
+        mLoginProgressBar = findViewById(R.id.login_progress_bar);
+
         mRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String mail = mMail.getText().toString();
-                final String password = mPassword.getText().toString();
+                final String mail = mMail.getText().toString().trim();
+                final String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mail) || TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(), "Some input fields empty",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mRegistrationProgressBar.setVisibility(View.VISIBLE);
+
                 mAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        mRegistrationProgressBar.setVisibility(View.GONE);
+
                         if(!task.isSuccessful()){
-                            Toast.makeText(DriverLoginActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(DriverLoginActivity.this, "Sign up error: " + task.getException(), Toast.LENGTH_SHORT).show();
                         }
                         else{
                             String user_id = mAuth.getCurrentUser().getUid();
@@ -77,11 +94,11 @@ public class DriverLoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 final String mail = mMail.getText().toString();
                 final String password = mPassword.getText().toString();
-                mAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(!task.isSuccessful()){
-                            Toast.makeText(DriverLoginActivity.this, "sign in error", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(DriverLoginActivity.this, "Sign in error.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
